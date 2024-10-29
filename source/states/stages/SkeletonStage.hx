5d0ec7b648cb5feb743ed466a4eccfa4eb893ce9
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-29 05:25:08 -0400

    Update SkeletonStage.hx

--- source/states/stages/SkeletonStage.hx
+++ source/states/stages/SkeletonStage.hx
@@ -4,7 +4,11 @@ import states.stages.objects.*;
 
 class SkeletonStage extends BaseStage
 {
-	var dad:Character3D;
+        public var dadMap:Map<String, Character3D> = new Map<String, Character3D>();
+	public var dad:Character3D = 'steve';
+	public var dadGroup:FlxSpriteGroup;
+
+	public var vocals:FlxSound;
 
 	override function create()
 	{
@@ -13,5 +17,31 @@ class SkeletonStage extends BaseStage
 			bg.active = false;
 			bg.screenCenter(XY);
 			add(bg);
+		
+		dadGroup = new FlxSpriteGroup(DAD_X, DAD_Y);
+		
+		dad = new Character3D(0, 0, SONG.player2);
+		startCharacterPos(dad, true);
+		dadGroup.add(dad);
+		
+		add(dadGroup);
+
+		startCharacterScripts(dad.curCharacter)
+	}
+	public function addCharacterToList(newCharacter:String, type:Int) {
+		switch(type) {
+			case 1:
+				if(!dadMap.exists(newCharacter)) {
+					var newDad:Character3D = new Character3D(0, 0, newCharacter);
+					dadMap.set(newCharacter, newDad);
+					dadGroup.add(newDad);
+					startCharacterPos(newDad, true);
+					newDad.alpha = 0.00001;
+					startCharacterScripts(newDad.curCharacter);
+				}
+	}
+	public function addBehindDad(obj:FlxBasic)
+	{
+		insert(members.indexOf(dadGroup), obj);
 	}
 }
