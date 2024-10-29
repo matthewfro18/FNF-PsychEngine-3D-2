a1820727b0df165db7c3d1bfea241cb8eab5ad43
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-29 05:21:54 -0400

    Update Minecraft.hx

--- source/states/stages/Minecraft.hx
+++ source/states/stages/Minecraft.hx
@@ -6,6 +6,9 @@ class Minecraft extends BaseStage
 {
         public var dadMap:Map<String, Character3D> = new Map<String, Character3D>();
 	public var dad:Character3D = 'steve';
+	public var dadGroup:FlxSpriteGroup;
+
+	public var vocals:FlxSound;
 
 	override function create()
 	{
@@ -17,5 +20,31 @@ class Minecraft extends BaseStage
 			bg.active = false;
 			bg.screenCenter(XY);
 			add(bg);
+		
+		dadGroup = new FlxSpriteGroup(DAD_X, DAD_Y);
+		
+		dad = new Character3D(0, 0, SONG.player2);
+		startCharacterPos(dad, true);
+		dadGroup.add(dad);
+		
+		add(dadGroup);
+
+		startCharacterScripts(dad.curCharacter)
+	}
+	public function addCharacterToList(newCharacter:String, type:Int) {
+		switch(type) {
+			case 1:
+				if(!dadMap.exists(newCharacter)) {
+					var newDad:Character3D = new Character3D(0, 0, newCharacter);
+					dadMap.set(newCharacter, newDad);
+					dadGroup.add(newDad);
+					startCharacterPos(newDad, true);
+					newDad.alpha = 0.00001;
+					startCharacterScripts(newDad.curCharacter);
+				}
+	}
+	public function addBehindDad(obj:FlxBasic)
+	{
+		insert(members.indexOf(dadGroup), obj);
 	}
 }
