275481aa80d6b9aaf72bad0cda35c007f98a4411
matthewfro1818 <155072293+matthewfro1818@users.noreply.github.com>
2024-10-29 05:25:29 -0400

    Update SkeletonStage.hx

--- source/states/stages/SkeletonStage.hx
+++ source/states/stages/SkeletonStage.hx
@@ -5,7 +5,7 @@ import states.stages.objects.*;
 class SkeletonStage extends BaseStage
 {
         public var dadMap:Map<String, Character3D> = new Map<String, Character3D>();
-	public var dad:Character3D = 'steve';
+	public var dad:Character3D = 'skeleton';
 	public var dadGroup:FlxSpriteGroup;
 
 	public var vocals:FlxSound;
